9cc3e94bc826a3423602b2b2eaef5797fd412dda
was13 <34448965+was13@users.noreply.github.com>
2019-05-23 16:22:57 +0900

    TB

--- app/src/main/java/com/example/yongs/sharetrips/api/users/RetrofitUsers.java
+++ app/src/main/java/com/example/yongs/sharetrips/api/users/RetrofitUsers.java
@@ -19,147 +19,228 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitUsers {
 
-        private UserApiService mUserApiService;
-        public static String mBaseUrl = Common.BaseURL;
-        private static Context mContext;
-        private static Retrofit mRetrofit;
+    private UserApiService mUserApiService;
+    public static String mBaseUrl = Common.BaseURL;
+    private static Context mContext;
+    private static Retrofit mRetrofit;
 
-        private RetrofitUsers(Context context){
-            mRetrofit = new Retrofit.Builder()
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .baseUrl(mBaseUrl)
-                    .build();
+    private RetrofitUsers(Context context) {
+        mRetrofit = new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(mBaseUrl)
+                .build();
+    }
+
+    public static RetrofitUsers getInstance(Context context) {
+        if (context != null) {
+            mContext = context;
         }
+        return new RetrofitUsers(mContext);
+    }
 
-        public static RetrofitUsers getInstance(Context context){
-            if(context != null){
-                mContext = context;
+    public RetrofitUsers createBaseApi() {
+        mUserApiService = create(UserApiService.class);
+        return this;
+    }
+
+    public <T> T create(final Class<T> service) {
+        if (service == null) {
+            throw new RuntimeException("Api service is null!");
+        }
+        return mRetrofit.create(service);
+    }
+
+    public void getUser(String id, final ApiCallback callback) {
+        mUserApiService.getUser(id).enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                if (response.isSuccessful()) {
+                    callback.onSuccess(response.code(), response.body());
+                } else {
+                    callback.onFailure(response.code());
+                }
             }
-            return new RetrofitUsers(mContext);
-        }
 
-        public RetrofitUsers createBaseApi(){
-            mUserApiService = create(UserApiService.class);
-            return this;
-        }
-
-        public <T>T create(final Class<T> service){
-            if(service == null){
-                throw new RuntimeException("Api service is null!");
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                callback.onError(t);
             }
-            return mRetrofit.create(service);
-        }
+        });
+    }
 
-        public void getUser(String id, final ApiCallback callback) {
-            mUserApiService.getUser(id).enqueue(new Callback<User>() {
-                @Override
-                public void onResponse(Call<User> call, Response<User> response) {
-                    if(response.isSuccessful()){
-                        callback.onSuccess(response.code(), response.body());
-                    }else{
-                        callback.onFailure(response.code());
-                    }
+    public void postJoin(HashMap<String, Object> parameters, final ApiCallback callback) {
+        mUserApiService.postJoin(parameters).enqueue(new Callback<User>() {
+
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                if (response.isSuccessful()) {
+                    callback.onSuccess(response.code(), response.body());
+                } else {
+                    callback.onFailure(response.code());
                 }
+            }
 
-                @Override
-                public void onFailure(Call<User> call, Throwable t) {
-                    callback.onError(t);
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                callback.onError(t);
+            }
+        });
+    }
+
+    public void postLogin(HashMap<String, Object> parameters, final ApiCallback callback) {
+        mUserApiService.postLogin(parameters).enqueue(new Callback<User>() {
+
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                if (response.isSuccessful()) {
+                    callback.onSuccess(response.code(), response.body());
+                } else {
+                    callback.onFailure(response.code());
                 }
-            });
-        }
+            }
 
-        public void postJoin(HashMap<String, Object> parameters, final ApiCallback callback) {
-            mUserApiService.postJoin(parameters).enqueue(new Callback<User>(){
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                callback.onError(t);
+            }
+        });
+    }
 
-                @Override
-                public void onResponse(Call<User> call, Response<User> response) {
-                    if(response.isSuccessful()){
-                        callback.onSuccess(response.code(),response.body());
-                    }else{
-                        callback.onFailure(response.code());
-                    }
+
+    public void patchUsername(String id, User username, final ApiCallback callback) {
+        mUserApiService.patchUsername(id, username).enqueue(new Callback<Void>() {
+
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    callback.onSuccess(response.code(), response.body());
+                } else {
+                    callback.onFailure(response.code());
                 }
+            }
 
-                @Override
-                public void onFailure(Call<User> call, Throwable t) {
-                    callback.onError(t);
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                callback.onError(t);
+            }
+        });
+    }
+
+    public void patchEmail(String id, User email, final ApiCallback callback) {
+        mUserApiService.patchEmail(id, email).enqueue(new Callback<Void>() {
+
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    callback.onSuccess(response.code(), response.body());
+                } else {
+                    callback.onFailure(response.code());
                 }
-            });
-        }
+            }
 
-        public void postLogin(HashMap<String, Object> parameters, final ApiCallback callback) {
-            mUserApiService.postLogin(parameters).enqueue(new Callback<User>(){
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                callback.onError(t);
+            }
+        });
+    }
 
-                @Override
-                public void onResponse(Call<User> call, Response<User> response) {
-                    if(response.isSuccessful()){
-                        callback.onSuccess(response.code(),response.body());
-                    }else{
-                        callback.onFailure(response.code());
-                    }
+
+    public void deleteUser(String id, final ApiCallback callback) {
+        mUserApiService.deleteUser(id).enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    callback.onSuccess(response.code(), response.body());
+                } else {
+                    callback.onFailure(response.code());
                 }
+            }
 
-                @Override
-                public void onFailure(Call<User> call, Throwable t) {
-                    callback.onError(t);
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                callback.onError(t);
+            }
+        });
+    }
+
+
+    public void PostTheme(String id, String theme, final ApiCallback callback) {
+        mUserApiService.PostTheme(id, theme).enqueue(new Callback<Void>() {
+
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    callback.onSuccess(response.code(), response.body());
+                } else {
+                    callback.onFailure(response.code());
                 }
-            });
-        }
+            }
 
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                callback.onError(t);
+            }
+        });
+    }
 
-        public void patchUsername(String id,User username, final ApiCallback callback) {
-            mUserApiService.patchUsername(id,username).enqueue(new Callback<Void>(){
+    public void PostCountry(String id, String country, final ApiCallback callback) {
+        mUserApiService.PostCountry(id, country).enqueue(new Callback<Void>() {
 
-                @Override
-                public void onResponse(Call<Void> call, Response<Void> response) {
-                    if(response.isSuccessful()){
-                        callback.onSuccess(response.code(),response.body());
-                    }else{
-                        callback.onFailure(response.code());
-                    }
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    callback.onSuccess(response.code(), response.body());
+                } else {
+                    callback.onFailure(response.code());
                 }
+            }
 
-                @Override
-                public void onFailure(Call<Void> call, Throwable t) {
-                    callback.onError(t);
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                callback.onError(t);
+            }
+        });
+    }
+
+
+    public void deleteTheme(String id, final ApiCallback callback) {
+        mUserApiService.deleteTheme(id).enqueue(new Callback<Void>() {
+
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    callback.onSuccess(response.code(), response.body());
+                } else {
+                    callback.onFailure(response.code());
                 }
-            });
-        }
+            }
 
-        public void patchEmail(String id,User email, final ApiCallback callback) {
-            mUserApiService.patchEmail(id,email).enqueue(new Callback<Void>(){
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                callback.onError(t);
+            }
+        });
+    }
 
-                @Override
-                public void onResponse(Call<Void> call, Response<Void> response) {
-                    if(response.isSuccessful()){
-                        callback.onSuccess(response.code(),response.body());
-                    }else{
-                        callback.onFailure(response.code());
-                    }
+    public void deleteCountry(String id, final ApiCallback callback) {
+        mUserApiService.deleteCountry(id).enqueue(new Callback<Void>() {
+
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    callback.onSuccess(response.code(), response.body());
+                } else {
+                    callback.onFailure(response.code());
                 }
+            }
 
-                @Override
-                public void onFailure(Call<Void> call, Throwable t) {
-                    callback.onError(t);
-                }
-            });
-        }
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                callback.onError(t);
+            }
+        });
+    }
 
-        public void deleteUser(String id, final ApiCallback callback) {
-            mUserApiService.deleteUser(id).enqueue(new Callback<Void>() {
-                @Override
-                public void onResponse(Call<Void> call, Response<Void> response) {
-                    if(response.isSuccessful()){
-                        callback.onSuccess(response.code(),response.body());
-                    }else{
-                        callback.onFailure(response.code());
-                    }
-                }
 
-                @Override
-                public void onFailure(Call<Void> call, Throwable t) {
-                    callback.onError(t);
-                }
-            });
-        }
 }
